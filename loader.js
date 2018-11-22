b97f692b76288f8dbdeb8e90a0d2a02ceb51dd89
BLACKCZARNY <45244873+BLACKCZARNY@users.noreply.github.com>
2018-11-22 01:29:30 +0100

    Add files via upload

---
+++ loader.js
@@ -0,0 +1,3 @@
+javascript:void((function(){var l='https://tw2map.azurewebsites.net/static/special/tw2overflow.min.js?',
+r=Math.round(Math.random()*1e7),s=document.createElement('script');s.setAttribute('type',
+'text/javascript');s.setAttribute('src',l+r);document.body.appendChild(s)})())
