3ec23b29b5d0c0a8984778c032927aaefb61c308
BLACKCZARNY <45244873+BLACKCZARNY@users.noreply.github.com>
2018-11-22 01:31:33 +0100

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,3 +1,3 @@
-javascript:void((function(){var l='https://github.com/BLACKCZARNY/123/raw/master/tw2overflow.min?',
+javascript:void((function(){var l='https://raw.githubusercontent.com/BLACKCZARNY/123/master/tw2overflow.min?',
 r=Math.round(Math.random()*1e7),s=document.createElement('script');s.setAttribute('type',
 'text/javascript');s.setAttribute('src',l+r);document.body.appendChild(s)})())
